972af4f7354875551e1a9e0fd8fa4bc697eff6d3
Zongjun Hu <zongjunhu@yahoo.com>
2018-10-07 14:38:20 -0400

    update java code

--- hello/Hello.java
+++ hello/Hello.java
@@ -3,7 +3,7 @@ package hello;
 public class Hello{
     public static void main(String[] args){
         for(int i=0; i<10; i++){
-            System.out.println("Hello No "+i);
+            System.out.println("Hello from No "+i);
         }
     }
 }
